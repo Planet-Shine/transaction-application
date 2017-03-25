3b188927f5363ce5936f071677a0610d6fba04a2
PavelOfficial <pavelofficial@ya.ru>
2017-03-25 18:22:45 +0300

    Unauthorised login form fix

--- src/redux/logout.js
+++ src/redux/logout.js
@@ -1,13 +1,16 @@
 
 import statusCodes from 'defs/httpStatusCodes';
 import {
+    LOGIN_FAILED,
     LOGOUT,
     logoutUser
 } from 'actions/account';
 
 export default function logoutMiddleware({dispatch}) {
     return next => action => {
-        if (action.payload && action.type !== LOGOUT && action.payload.statusCode === statusCodes.Unauthorized) {
+        if (action.payload &&
+                !~[LOGOUT, LOGIN_FAILED].indexOf(action.type) &&
+                action.payload.statusCode === statusCodes.Unauthorized) {
             let {error, statusCode} = action.payload;
             dispatch(logoutUser(error, statusCode));
         }
